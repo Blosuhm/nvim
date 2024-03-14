4dc76e65d5be616c92c76e4f5f184cb2955794c5
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2024-03-14 15:23:03 +0000

    added formatting for zsh

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -12,6 +12,7 @@ return {
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         sh = { { 'beautysh', 'shfmt' } },
+        zsh = { { 'beautysh', 'shfmt' } },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
